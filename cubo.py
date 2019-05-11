04ee42fd50a548c3a16b706b3cf39bacaa88edb5
Adriano Silva <adrianosilvaoliveiradejesus@gmail.com>
2019-05-11 20:07:13 -0300

    0.1

--- cubo.py
+++ cubo.py
@@ -17,22 +17,28 @@ argumentos = sys.argv
 #instânciando objeto Algoritmos
 algoritmos = Algoritmos()
 
+#Se o segundo argumento da lista é "OLL" ele cai nesse bloco
 if(argumentos[1] == "OLL"):
+    #argumentos[2] == referência da OLL
+    #argumentos[3] == "Se o Algortimo é reverso ou não"
     try:
+        #O argumento '-R' quer dizer para printar o algorimo invertido
         if(argumentos[2] == "-R"):
             print("Algoritmo Reverso")
+    #Esse bloco printa na tela o algoritmo com a referência do mesmo
     except Exception as e:
         print("OLL")
 
+#Se não,se ele for igual "PLL" Ele cai nesse bloco
 elif(argumentos[1] == "PLL"):
+    #argumentos[2] == referência da PLL
+    #argumentos[3] == "Se o Algortimo é reverso ou não"
     try:
-        if(argumentos[2] == "-R"):
-            print("Algoritmo Reverso")
+        #O argumento '-R' quer dizer para printar o algorimo invertido
+        if(argumentos[3] == "-R"):
+            saida = reverso(algoritmos.PLL[argumentos[2]])
+            print(saida)
+    #Esse bloco printa na tela o algoritmo com a referência do mesmo
     except Exception as e:
-        print(algoritmos.PLL[argumentos[2]])
-
-
-saida = reverso("Adriano")
-
-for move in saida:
-    print(move)
+        saida = algoritmos.PLL[argumentos[2]]
+        print(saida)
